dfcb7a3b91ab432bd014ea323f25a0c46f69412e
Vinuri Rodrigo <vinuri.2019753@iit.ac.lk>
2024-07-27 23:31:49 +0530

    edit doc api

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT}`,
+        url: `${process.env.DOC}`,
       },
     ],
     components: {
